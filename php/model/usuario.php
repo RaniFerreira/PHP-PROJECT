4cec6645d38e0d09bc66cbada4837c09c8680f37
RaniFerreira <franielly535@gmail.com>
2024-07-17 15:23:01 -0300

    adicionando base de dados e logica em php

---
+++ php/model/usuario.php
@@ -0,0 +1,29 @@
+<?php
+
+    class Usuario{
+
+        public function addUsuario($email, $nome, $senha){
+
+            try{
+                $sql = "INSERT INTO usuario values (?,?,?,?,?,?)";
+                $stmt = Conexao::getConexao()->prepare($sql);
+                $stmt -> bindValue(1,$email);
+                $stmt -> bindValue(2,md5($email));
+                $stmt -> bindValue(3,$nome);
+                $stmt -> bindValue(4,$senha);
+                $stmt -> bindValue(5,date('y-m-d'));
+                $stmt -> bindValue(6,'padrao.jpg');
+                $stmt -> execute();
+
+                return 'Usuario cadastrado com sucesso!';
+            }catch (PDOException $ex){
+                if($ex->errorInfo[1] == 1062){
+                    return 'Usuario já existente';
+                }else{
+                    return 'Erro ao cadastrar usuário';
+                }
+            }
+        }
+    }
+
+?>
